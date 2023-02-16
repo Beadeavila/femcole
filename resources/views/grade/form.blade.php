762fac1432dc49bfda027611e2c71d831ce43f36
Beadeavila <beadeavila10@gmail.com>
2023-02-16 13:19:53 +0100

    select options added to grades form

--- resources/views/grade/form.blade.php
+++ resources/views/grade/form.blade.php
@@ -9,17 +9,17 @@
 
         <div class="form-group">
             {{ Form::label('subject') }}
-            {{ Form::text('subject', $grade->subject, ['class' => 'form-control' . ($errors->has('subject') ? ' is-invalid' : ''), 'placeholder' => 'Subject']) }}
+            {{ Form::select('subject',['lengua'=>'Lengua','inglés'=>'Inglés','matemáticas'=>'Matemáticas', 'historia'=>'Historia' ,'geografía'=>'Geografía'], $grade->subject, ['class' => 'form-control' . ($errors->has('subject') ? ' is-invalid' : ''), 'placeholder' => 'Subject']) }}
             {!! $errors->first('subject', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('trimester') }}
-            {{ Form::text('trimester', $grade->trimester, ['class' => 'form-control' . ($errors->has('trimester') ? ' is-invalid' : ''), 'placeholder' => 'Trimester']) }}
+            {{ Form::select('trimester',['1'=>'1','2'=>'2','3'=>'3'], $grade->trimester, ['class' => 'form-control' . ($errors->has('trimester') ? ' is-invalid' : ''), 'placeholder' => 'Trimester']) }}
             {!! $errors->first('trimester', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('exam') }}
-            {{ Form::text('exam', $grade->exam, ['class' => 'form-control' . ($errors->has('exam') ? ' is-invalid' : ''), 'placeholder' => 'Exam']) }}
+            {{ Form::select('exam',['1'=>'1','2'=>'2','3'=>'3'], $grade->exam, ['class' => 'form-control' . ($errors->has('exam') ? ' is-invalid' : ''), 'placeholder' => 'Exam']) }}
             {!! $errors->first('exam', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
@@ -29,7 +29,7 @@
         </div>
         <div class="form-group">
             {{ Form::label('schoolYear') }}
-            {{ Form::text('schoolYear', $grade->schoolYear, ['class' => 'form-control' . ($errors->has('schoolYear') ? ' is-invalid' : ''), 'placeholder' => 'Schoolyear']) }}
+            {{ Form::text('schoolYear', $grade->schoolYear, ['class' => 'form-control' . ($errors->has('schoolYear') ? ' is-invalid' : ''), 'placeholder' => '2023']) }}
             {!! $errors->first('schoolYear', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 

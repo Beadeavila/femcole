aafbde39c984135907d284fee816401e72befcb2
Beadeavila <beadeavila10@gmail.com>
2023-02-16 13:53:14 +0100

    changed naming into spanish

--- resources/views/grade/form.blade.php
+++ resources/views/grade/form.blade.php
@@ -2,39 +2,39 @@
     <div class="box-body">
 
         <div class="form-group">
-            {{ Form::label('name') }}
-            {{ Form::select('user_id', $user, $grade->user_id, ['class' => 'form-control' . ($errors->has('user_id') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {{ Form::label('nombre') }}
+            {{ Form::select('user_id', $user, $grade->user_id, ['class' => 'form-control' . ($errors->has('user_id') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
             {!! $errors->first('user_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
         <div class="form-group">
-            {{ Form::label('subject') }}
-            {{ Form::select('subject',['lengua'=>'Lengua','inglés'=>'Inglés','matemáticas'=>'Matemáticas', 'historia'=>'Historia' ,'geografía'=>'Geografía'], $grade->subject, ['class' => 'form-control' . ($errors->has('subject') ? ' is-invalid' : ''), 'placeholder' => 'Subject']) }}
+            {{ Form::label('asignatura') }}
+            {{ Form::select('subject',['lengua'=>'Lengua','inglés'=>'Inglés','matemáticas'=>'Matemáticas', 'historia'=>'Historia' ,'geografía'=>'Geografía'], $grade->subject, ['class' => 'form-control' . ($errors->has('subject') ? ' is-invalid' : ''), 'placeholder' => 'Asignatura']) }}
             {!! $errors->first('subject', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('trimester') }}
-            {{ Form::select('trimester',['1'=>'1','2'=>'2','3'=>'3'], $grade->trimester, ['class' => 'form-control' . ($errors->has('trimester') ? ' is-invalid' : ''), 'placeholder' => 'Trimester']) }}
+            {{ Form::label('trimestre') }}
+            {{ Form::select('trimester',['1'=>'1','2'=>'2','3'=>'3'], $grade->trimester, ['class' => 'form-control' . ($errors->has('trimester') ? ' is-invalid' : ''), 'placeholder' => 'Trimestre']) }}
             {!! $errors->first('trimester', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('exam') }}
-            {{ Form::select('exam',['1'=>'1','2'=>'2','3'=>'3'], $grade->exam, ['class' => 'form-control' . ($errors->has('exam') ? ' is-invalid' : ''), 'placeholder' => 'Exam']) }}
+            {{ Form::label('examen') }}
+            {{ Form::select('exam',['1'=>'1','2'=>'2','3'=>'3'], $grade->exam, ['class' => 'form-control' . ($errors->has('exam') ? ' is-invalid' : ''), 'placeholder' => 'Examen']) }}
             {!! $errors->first('exam', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('grade') }}
-            {{ Form::text('grade', $grade->grade, ['class' => 'form-control' . ($errors->has('grade') ? ' is-invalid' : ''), 'placeholder' => 'Grade']) }}
+            {{ Form::label('nota') }}
+            {{ Form::text('grade', $grade->grade, ['class' => 'form-control' . ($errors->has('grade') ? ' is-invalid' : ''), 'placeholder' => 'Nota']) }}
             {!! $errors->first('grade', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('schoolYear') }}
+            {{ Form::label('curso escolar') }}
             {{ Form::text('schoolYear', $grade->schoolYear, ['class' => 'form-control' . ($errors->has('schoolYear') ? ' is-invalid' : ''), 'placeholder' => '2023']) }}
             {!! $errors->first('schoolYear', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
     </div>
 </div>

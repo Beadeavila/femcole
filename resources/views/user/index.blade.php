9a830953e2f50d3b29765aa9d0c47656cdfb1142
Adriana Aguilar <agruad@gmail.com>
2023-02-17 14:04:01 +0100

    merged develop to average

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -21,7 +21,7 @@
                     @if ($message = Session::get('success'))
                         <div class="alert alert-success">
                             <p>{{ $message }}</p>
-                        </div>
+                        </div> 
                     @endif
 
                     <div class="tableContainer">

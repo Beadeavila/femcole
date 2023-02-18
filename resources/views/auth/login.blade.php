05f1183023381d84b530c0195dc074c598e4a455
Adriana Aguilar <agruad@gmail.com>
2023-02-18 12:56:02 +0100

    changed language in login view, changed browser tab title and muted register option

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <section class="content container-fluid">
     <div class="cardFem">
-        <div class="cardTitle">{{ __('Login') }}</div>
+        <div class="cardTitle">{{ __('INICIO DE SESIÓN') }}</div>
             <div class="cardBbody">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="form-group">
                             <div class="colLabel">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo Electrónico') }}</label>
                             </div>
                             <div class="colInput">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -24,7 +24,7 @@
                     </div>
                     <div class="form-group">
                         <div class="colLabel">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
                         </div>
                         <div class="colInput">
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -43,7 +43,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recuérdame') }}
                                     </label>
                                 </div>
                             </div>
@@ -52,12 +52,12 @@
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn greenbtn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar sesión') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('¿Olvidaste tu contraseña?') }}
                                     </a>
                                 @endif
                             </div>
